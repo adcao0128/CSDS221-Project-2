36bc65532ead6ad54b08a1f8996636068fb7af9b
adcao0128 <98364256+adcao0128@users.noreply.github.com>
2022-11-19 18:22:42 -0500

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+//Imports
 import React from "react";
 import "./style.css";
+//More imports
+import Card from '@mui/material/Card';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography'
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+        <Card sx={{
+          m: -1.2,
+          width: 'auto',
+          height: 50,
+          color: 'white',
+          bgcolor: 'primary.dark',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+          <MenuIcon />
+          <Typography variant='button'>
+            FRAMEWORKS
+          </Typography>
+        </Card>
     </div>
   );
 }
